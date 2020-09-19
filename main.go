752ddb38fcd2977811fa161d55a727a9c3464311
Sulthan Alam <sulam3010@gmail.com>
2020-09-19 11:45:14 +0700

    [alam] success post method

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -9,11 +10,16 @@ import (
 var url = "https://ditsti.itb.ac.id/nic/manajemen_akun/pengecekan_user"
 
 //paste your cookie here, format(19-09-2020): ci_session=xxxxxx)
-var cookie = "ci_session=4uimcpvsdfkad4eealjgfpn255fj9dh3"
+var session = "hcpn1ljlqvs20sktd3b560uejj1v61d4"
+var nic = "ff2f1058a1f91f384f38f9af83b2bef2"
+var cookie = "ITBnic=" + nic + "; ci_session=" + session
+var payload = "NICitb=" + nic + "&uid=18119004"
 
 func main() {
-	req, _ := http.NewRequest("GET", url, nil)
+	req, _ := http.NewRequest("POST", url, bytes.NewBufferString(payload))
 	req.Header.Add("Cookie", cookie)
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
 	client := http.Client{}
 
 	res, err := client.Do(req)
